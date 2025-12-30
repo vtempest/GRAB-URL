4b48e93dcf07917993f4ddd595e7544b79d7d1f8
vtempest <you@example.com>
2025-12-30 13:27:44 -0800

    icon fix

--- docs/app/customize-docs.ts
+++ docs/app/customize-docs.ts
@@ -1,6 +1,6 @@
 export const title = "GRAB URL Docs",
   description = "GRAB URL Lib",
-  logo = "./apple-touch-icon.png",
+  logo = "/apple-touch-icon.png",
   github = "https://github.com/vtempest/grab-url",
-  favicon = "./favicon.ico";
+  favicon = "/favicon.ico";
 
